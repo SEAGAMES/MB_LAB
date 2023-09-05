190f2904f04e9a0b17c5866694ac61b4f757fc37
DevGame <t.seagames@hotmail.com>
2023-09-05 15:22:07 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,92 @@
+<template>
+  <router-view />
+</template>
+
+<script setup>
+//=
+</script>
+
+<style>
+@font-face {
+  font-family: "Prompt";
+  src: url("./assets/Font/Prompt/Prompt-Regular.ttf") format("truetype");
+  font-style: normal;
+}
+
+@font-face {
+  font-family: "Sarabun";
+  src: url("./assets/Font/Sarabun/Sarabun-Regular.ttf") format("truetype");
+  font-style: normal;
+}
+
+.fontSarabun {
+  font-family: "Sarabun", sans-serif;
+}
+
+.fontSize4 {
+  font-size: 4px;
+}
+
+.fontSize6 {
+  font-size: 6px;
+}
+
+.fontSize8 {
+  font-size: 8px;
+}
+
+.fontSize9 {
+  font-size: 9px;
+}
+
+.fontSize10 {
+  font-size: 10px;
+}
+
+.fontSize11 {
+  font-size: 11px;
+}
+.fontSize12 {
+  font-size: 12px;
+}
+
+.fontSize13 {
+  font-size: 13px;
+}
+
+.fontSize14 {
+  font-size: 14px;
+}
+
+.fontSize16 {
+  font-size: 16px;
+}
+
+.fontSize18 {
+  font-size: 18px;
+}
+
+.fontSize20 {
+  font-size: 20px;
+}
+
+.fontSize22 {
+  font-size: 22px;
+}
+
+.fontSize24 {
+  font-size: 24px;
+}
+
+.fontSize26 {
+  font-size: 26px;
+}
+
+.fontSize28 {
+  font-size: 28px;
+}
+
+.fontSize30 {
+  font-size: 30px;
+}
+</style>
---
+++ src/views/MB-Lab.vue
@@ -0,0 +1,120 @@
+<template>
+  <v-container class="fontSarabun">
+    <v-row>
+      <v-spacer></v-spacer>
+      <h1 class="text-bold mt-3">ระบบจองห้อง Lab</h1>
+      <v-spacer></v-spacer>
+    </v-row>
+
+    <!-- v-card input data -->
+    <v-form ref="form" lazy-validation>
+      <v-card class="mx-auto my-7" width="900" height="400">
+        <v-card-text>
+          <v-row class="fontSize18"
+            ><v-col>
+              <div align="center" class="mt-3">
+                <p>ผู้จอง : {{ form.rent_name }}</p>
+              </div>
+            </v-col>
+
+            <v-col>
+              <div align="center">
+                <v-text-field
+                  label="เบอร์ติดต่อ"
+                  v-model="form.telNo"
+                  prepend-inner-icon="mdi-phone-plus"
+                  :rules="telNoRules"
+                  single-line
+                  type="number"
+                  autofocus
+                  required
+                  outlined
+                  dense
+                ></v-text-field></div
+            ></v-col>
+          </v-row>
+          <v-row>
+            <v-col>
+              <div>
+                <v-select
+                  :rules="floorRules"
+                  label="โซน"
+                  required
+                  outlined
+                  dense
+                  :items="['A', 'B', 'C', 'D']"
+                  variant="outlined"
+                ></v-select></div
+            ></v-col>
+            <v-col>
+              <div>
+                <v-select
+                  :rules="floorRules"
+                  label="ชั้น"
+                  outlined
+                  dense
+                  :items="['1', '2', '3', '4']"
+                  variant="outlined"
+                ></v-select></div
+            ></v-col>
+            <v-col>
+              <div>
+                <v-select
+                  :rules="floorRules"
+                  label="ห้อง"
+                  outlined
+                  dense
+                  :items="['101', '102', '103', '104']"
+                  variant="outlined"
+                ></v-select></div
+            ></v-col>
+          </v-row>
+          <v-row class="fontSize18 mt-2">
+            <v-col>
+              <div>
+                <p>ช่วงเวลาที่ต้องการจอง :</p>
+              </div></v-col
+            >
+            <v-col><v-text-field dense></v-text-field></v-col>
+          </v-row>
+          <v-row>
+            <v-btn
+              @click="validate"
+              class="mt-11"
+              color="green"
+              append-icon="mdi-check-circle"
+              block
+              >Block Button</v-btn
+            >
+          </v-row>
+        </v-card-text>
+      </v-card>
+    </v-form>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    form: {
+      rent_name: "ธนกฤต นิ่มนวล",
+      telNo: "",
+    },
+    telNoRules: [
+      (v) => !!v || "กรุณาใส่เบอร์โทรศัพท์",
+      //(v) => v >= 9 || "Height is more than 1",
+    ],
+    floorRules: [
+      (v) => !!v || "กรุณาระบุห้องให้ชัดเจน",
+      //(v) => v >= 9 || "Height is more than 1",
+    ],
+  }),
+
+  methods: {
+    async validate() {
+      this.$refs.form.validate();
+      //console.log(this.$refs.form.validate());
+    },
+  },
+};
+</script>
